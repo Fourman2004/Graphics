5caadb87a9d7b7ce9534be74a2355431d319246c
Marshall Sharp <76953814+Fourman2004@users.noreply.github.com>
2025-03-10 17:12:09 +0000

    Add shader.h file

--- OpenGL/VertexShader.glsl
+++ OpenGL/VertexShader.glsl
@@ -4,5 +4,5 @@ layout (location = 0) in vec3 Pos;
 
 void main()
 {
-    gl_Position = vec4(Pos.x, Pos.y, Pos.z, 1.0);
+    gl_Position = vec4(Pos.x, -Pos.y, Pos.z, 1.0);
 }
--- OpenGL/main.h
+++ OpenGL/main.h
@@ -14,7 +14,7 @@ const char* vertShadSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
 "void main()\n"
 "{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"   gl_Position = vec4(aPos.x, -aPos.y, aPos.z, 1.0);\n"
 "}\0";
 
 const char* fragShadSource = "out vec4 FragColor;\n "
