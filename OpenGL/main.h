7cad1ee75eb56d302337542b4dd8b7bc7ed63ca2
Marshall Sharp <76953814+Fourman2004@users.noreply.github.com>
2025-03-10 23:44:14 +0000

    clean up main header

--- OpenGL/main.h
+++ OpenGL/main.h
@@ -12,19 +12,6 @@ typedef void (*GL_GENBUFFERS) (GLsizei, GLuint*);
 unsigned int buffer;
 GLuint vertexShader, fragmentShader,shaderProgram, VAO, VBO, EBO;
 
-const char* vertShadSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos.x, -aPos.y, aPos.z, 1.0);\n"
-"}\0";
-
-const char* fragShadSource = "out vec4 FragColor;\n "
-"void main()\n"
-"{\n"
-    "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f); \n"
-"}\n";
-
 void inputProcess(GLFWwindow* window);
 bool WindowGen();
 void callbackFramebufferSize(GLFWwindow* window, int width, int height);
