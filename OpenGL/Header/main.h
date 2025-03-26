c0adcc7bc7b3b3a85f9fabdfe40ace9dc8143cfe
Marshall Sharp <76953814+Fourman2004@users.noreply.github.com>
2025-03-26 14:38:34 +0000

    Added particle class

--- OpenGL/Header/main.h
+++ OpenGL/Header/main.h
@@ -52,3 +52,13 @@ bool WindowGen();
 /// Creates the Shape by drawing verticies in the window. Passes the data through VBO, VAO and EBO buffers.
 /// </summery>
 int shapeGen();
+
+struct Particle
+{
+	glm::vec3 Position, Velocity;
+	glm::vec4 Color;
+	float Life;
+	Particle()
+		: Position(0.0f), Velocity(0.0f), Color(1.0f), Life(0.0f) {
+	}
+};
